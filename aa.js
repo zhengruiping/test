3259f95af6b6799f4caa5cb98e90cc68e4a3c21d
zhengruiping1989 <1250295060@qq.com>
2019-12-18 17:40:21 +0800

    Update aa.js

--- aa.js
+++ aa.js
@@ -1,10 +1,1 @@
-
-console.log('ok')
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-console.log('ok')
->>>>>>> Stashed changes
-=======
-
->>>>>>> d92ef8ca796e60acefd2e3e113eef6a2ebf52eb0
+console.log('bb')
